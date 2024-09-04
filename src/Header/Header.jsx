1099abcc07c592e2071229044cfd1c613cc26bd2
Theo <theo.sohlman@gmail.com>
2024-09-04 14:44:14 +0200

    Deployment done

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -6,12 +6,12 @@ function Header() {
         <header>
             <nav className={styles.navbar}>
                 <a href="#">Theo S.</a>
-                <i className={styles.material-icons}>menu</i>
-                <div className="nav-container">
-                    <div className="close-container">
-                        <i className="material-icons, closeBtn">close</i>
+                <i className={`material-icons ${styles.menuBtn}`}>menu</i>
+                <div className={styles.navContainer}>
+                    <div className={styles.closeContainer}>
+                        <i className={`material-icons ${styles.closeBtn}`}>close</i>
                     </div>
-                    <ul className="nav-list">
+                    <ul className={styles.navList}>
                         <li>
                             <a href="#">Intro</a>
                         </li>
