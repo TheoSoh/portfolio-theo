e01924786ab2b256cdbd5bfa865d6bee3f1e2c51
Theo <theo.sohlman@gmail.com>
2024-09-11 17:16:05 +0200

    total reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import Header from './Header/Header.jsx'
-import Intro from './Intro/Intro.jsx';
-import SeparationLine from './SeparationLine.jsx';
 
 
 function App() {
 
   return (
     <>
-      <Header/>
-      <Intro/>
-      <SeparationLine/>
-      <Intro/>
+      
     </>
   );
 }
