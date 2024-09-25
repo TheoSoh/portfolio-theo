7938e894ac80bede3361d7e651b1f3d826e16e36
Theo <theo.sohlman@gmail.com>
2024-09-25 10:08:03 +0200

    swapped observer for a custom hook observer

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,40 +1,19 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import AnchorToGitHub from "./AnchorToGithub"
+import useFadeInIntersectionObserver from './useFadeInIntersectionObserver';
 
  function Project({ lgStyle, title, subtitle, textBody, href, createdUsingIcons, vcsIcon, hostedWithIcons }) {
   const [isVisible, setIsVisible] = useState(false); // Track visibility state
   const ref = useRef(null); // Create a ref for the component
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setIsVisible(true); // Set visibility when it first enters the viewport
-            observer.unobserve(entry.target); // Stop observing after the first intersection
-          }
-        });
-      },
-      {
-        threshold: 0.1, // Trigger when 10% of the component is visible
-      }
-    );
-
-    if (ref.current) {
-      observer.observe(ref.current); // Start observing the component
-    }
-
-    return () => {
-      if (ref.current) observer.unobserve(ref.current); // Cleanup observer on unmount
-    };
-  }, []);
+  
+  useFadeInIntersectionObserver(ref, setIsVisible)
   
   return (
     <div
     ref={ref}
     className={`flex flex-col bg-gray w-full h-fit relative 
     px-3 pt-2 pb-3 border border-graphite rounded-md 
-    ${lgStyle ? lgStyle:''} fade-in ${isVisible ? 'visible' : ''}`}
+    ${lgStyle ? lgStyle:''} ${isVisible ? 'fade-in-y' : 'opacity-0'}`}
     >
       <h2 className='font-mono'>
         <b>{title}</b> {subtitle && (<span className='text-xs'>{subtitle}</span>)}
@@ -42,11 +21,13 @@ import AnchorToGitHub from "./AnchorToGithub"
       <p className=' pt-2 pb-4'
       dangerouslySetInnerHTML={{ __html: textBody }}
       />
-      <AnchorToGitHub href={href}/>
+      <AnchorToGitHub link={href}/>
       <p className='font-mono text-sm pt-4 pb-2'>CREATED USING</p>
       <div className='flex flex-wrap gap-3'>
         {createdUsingIcons.map((icon, index) => (
-          <img key={index} src={icon} className='h-8 max-w-8'/>
+          <img key={index} src={icon} draggable='false' 
+          alt='project-icon' className='h-8 max-w-8'
+          />
         ))}
       </div>
       {vcsIcon && (
