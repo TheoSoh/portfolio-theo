79786108d6f868018323b218273768298c200061
Theo <theo.sohlman@gmail.com>
2024-09-25 10:03:53 +0200

    removed navmenu component from app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useRef } from 'react'
 import { SectionsContext } from './contexts/sectionsContext'
-import NavMenu from './components/NavMenu'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
@@ -22,8 +21,7 @@ function App() {
 
   return (
     <SectionsContext.Provider value={sections}>
-      <NavMenu />
-      <div className='flex justify-center py-28 px-[15%] md:px-[10%] w-full text-graphite'>
+      <div className='flex justify-center py-[15vh] px-[15%] md:px-[10%] w-full text-graphite'>
         <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-[84.575rem]">
           <div className='w-full h-fit mb-16'>
             <Hero />
