435962b39c2f838bbe01758ce99e0c1caa1c5861
Theo <theo.sohlman@gmail.com>
2024-10-06 14:53:22 +0200

    replaced form with contact details

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,98 +1,85 @@
 import { useState, useRef } from 'react';
-import axios from 'axios';
-import InputField from '../components/InputField'
-import TextAreaField from '../components/TextAreaField'
 import useFadeInIntersectionObserver from '../components/useFadeInIntersectionObserver';
+import clipboardCopyIcon from '../assets/contact_icons/clipboard-copy.svg'
+import clipboardCheckIcon from '../assets/contact_icons/clipboard-check.svg'
+import linkIcon from '../assets/contact_icons/link.svg'
 
 const Contact = ({ sectionRef }) => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  })
-  const [isSubmitting, setIsSubmitting] = useState(false)
-  const [responseMessage, setResponseMessage] = useState('')
-
-  // Handle form input changes
-  const handleChange = (e) => {
-    const { name, value } = e.target
-    setFormData({
-      ...formData,
-      [name]: value,
-    })
-  }
-
-  // Handle form submission
-  const handleSubmit = async (e) => {
-    e.preventDefault() // Prevent the default form submit action
-    setIsSubmitting(true)
-
-    try {
-      // Make the POST request to the API Gateway endpoint
-      const response = await axios.post('https://upgf4n2hfg.execute-api.eu-north-1.amazonaws.com/prod/send-email', formData)
-      
-      setResponseMessage('Message sent successfully!')
-      console.log('Email sent successfully:', response.data)
-    } catch (error) {
-      setResponseMessage('Failed to send message.')
-      console.error('Error sending email:', error)
-    } finally {
-      setIsSubmitting(false) // Stop loading state
-    }
-  }
-
   const [isVisible, setIsVisible] = useState(false)
   const formRef = useRef(null)
   useFadeInIntersectionObserver(formRef, setIsVisible)
 
+  const [isCopied, setIsCopied] = useState(false)
+  
+  const writeToClipboard = async () => {
+    const emailText = document.getElementById('emailToCopy').textContent
+    await navigator.clipboard.writeText(emailText).then(() => {
+      console.log(`'${emailText}' is copied to clipboard.`)
+      setIsCopied(true)
+      // Resolved - text copied to clipboard successfully
+    }, () => {
+      console.error('Failed to copy.')
+      setIsCopied(false)
+      // Rejected - text failed to copy to the clipboard
+    })
+  }
+  
+  const checkClipboardWritePermission = async () => {
+    navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+      if (result.state == "granted" || result.state == "prompt") {
+        writeToClipboard()
+      } else {
+        console.log('Insufficient clipboard-write permission.')
+      }
+    })
+  }
+
   return (
     <section ref={sectionRef} className='flex flex-col w-full gap-9'>
-      <h1>Feel free to send me a message below...</h1>
-      <form
+      <h1>Feel free to send me a message...</h1>
+      <div
       ref={formRef}
-      onSubmit={handleSubmit}
       className={`flex flex-col w-full lg:w-2/3 min-w-fit 
-      bg-gray border rounded-md p-8 gap-6
+      bg-gray border border-graphite rounded-md p-3 gap-6
       ${isVisible ? 'fade-in-y' : 'opacity-0'}`}
       >
-        <h2 className='font-mono font-semibold'>Send me a message!</h2>
-        <InputField 
-        label="Name:"
-        type='text'
-        name='name'
-        value={formData.name}
-        onChange={handleChange}
-        placeholder={'Theo Sohlman'}
-        />
-        <InputField 
-        label='Email:'
-        type='email'
-        name='email'
-        value={formData.email}
-        onChange={handleChange}
-        placeholder='theo.sohlman@gmail.com'
-        />
-        <TextAreaField 
-        label='Message:'
-        name='message'
-        value={formData.message}
-        onChange={handleChange}
-        placeholder='Hello there...'
-        />
-        <div className='flex flex-wrap gap-6'>
-          <button 
-          type='submit'
-          disabled={isSubmitting}
-          className='bg-green w-fit px-6 border rounded-md 
-          hover:bg-gradient-to-t from-light-green to-green'
-          >
-            {isSubmitting ? 'Sending...' : 'Send'}
-          </button>
-          {responseMessage && <p className='font-semibold'>{responseMessage}</p>}
+        <h2 className='font-mono font-bold'>CONCTACT INFORMATION</h2>
+        <div>
+          <h3 className='font-mono font-medium'>Email:</h3>
+          <div className='flex flex-wrap gap-2'>
+            <p id='emailToCopy'>theo.sohlman@gmail.com</p>
+            <button
+            type='button'
+            aria-label='Copy email'
+            onClick={() => {checkClipboardWritePermission()}}
+            className='flex items-center font-mono font-medium text-xs text-green active:text-light-green'
+            >
+              <img 
+              src={isCopied ? clipboardCheckIcon : clipboardCopyIcon}
+              alt='clipboard icon'
+              className='h-4 w-auto'
+              />
+              {isCopied ? 'Copied' : 'Copy'}
+            </button>
+          </div>
         </div>
-      </form>
+        <div>
+          <h3 className='font-mono font-medium'>Phone:</h3>
+          <p>(+46) 070-689 38 39</p>
+        </div>
+        <a 
+        href={'mailto:theo.sohlman@gmail.com'}
+        target='_blank'
+        rel='noopener noreferrer'
+        aria-label='Send an email'
+        className='flex gap-1 w-fit font-mono font-medium text-sm 
+        text-green active:text-light-green'
+        >
+          <img src={linkIcon} alt='link icon' className='w-5 h-auto'/>
+          Send an email
+        </a>
+      </div>
     </section>
   )
 }
-/*re_dv9MPXCk_La9hjB5f2Hpb3eEyiaSv1gqQ*/
 export default Contact
