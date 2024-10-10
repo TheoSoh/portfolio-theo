fe6c30daf9ecdd148eb876965dd8e47587461bae
Theo <theo.sohlman@gmail.com>
2024-10-10 14:31:11 +0200

    swapped "key" and "software"

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,8 +7,8 @@ const About = ({ sectionRef }) => {
     h-fit text-justify lg:text-left pb-20 md:pb-16 2xl:pb-28'
     >
       <p className='font-mono'>A little about me...</p>
-      <p>I’m currently in my <b>fifth semester</b> at Örebro University (Sweden), enrolled in the <b>Programme of Systems Analysis</b> (Systemvetenskapliga programmet). I have always had a strong connection to technology, with computers playing a key role in my life from a young age.</p>
-      <p>Over the past two years, my academic journey has solidified my <b>passion</b> for problem-solving and building user-centric, robust and scalable systems. I thrive in environments where complex challenges need elegant and efficient solutions. <b>My goal</b> is to leverage my skills and knowledge to create impactful software that not only solves real-world problems but also enhances user experiences.</p>
+      <p>I’m currently in my <b>fifth semester</b> at Örebro University (Sweden), enrolled in the <b>Programme of Systems Analysis</b> (Systemvetenskapliga programmet). I have always had a strong connection to technology, with computers playing a significant role in my life from a young age.</p>
+      <p>Over the past two years, my academic journey has solidified my <b>passion</b> for problem-solving and building user-centric, robust and scalable systems. I thrive in environments where complex challenges need elegant and efficient solutions. <b>My goal</b> is to leverage my skills and knowledge to create impactful solutions that not only solves real-world problems but also enhances user experiences.</p>
       <p>My <b>attention to detail</b> ensures that the work I produce is not only functional but also meticulously crafted to meet high standards of usability and quality.</p>
       <p>As I look toward <b>the next step</b> in my career, I’m excited to bring my commitment to excellence, eye for detail, and technical expertise into a professional setting.</p>
     </section>
