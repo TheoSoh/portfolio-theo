c245ddd3fd64fae7f1971e140a4bbaad59b38a71
Theo Sohlman <theo.sohlman@gmail.com>
2024-11-05 16:45:05 +0100

    edit text position

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ const About = ({ sectionRef }) => {
   return (
     <section
       ref={sectionRef}
-      className="scroll-target flex h-fit w-full flex-col gap-8 pb-20 text-justify md:pb-16 lg:text-left 2xl:pb-28"
+      className="scroll-target flex h-fit w-full flex-col gap-8 pb-20 md:pb-16 2xl:pb-28"
     >
       <p className="font-mono">A little about me...</p>
       <p>
@@ -20,11 +20,11 @@ const About = ({ sectionRef }) => {
       </p>
       <p>
         My <b>attention to detail</b> ensures that the work I produce is not only functional but
-        also meticulously crafted to meet high standards of usability and quality.
+        also meticulously crafted to meet high standards of usability and quality. As I look toward <b>the next step</b> in my career, I’m excited to bring my commitment to
+        excellence, eye for detail, and technical expertise into a professional setting.
       </p>
       <p>
-        As I look toward <b>the next step</b> in my career, I’m excited to bring my commitment to
-        excellence, eye for detail, and technical expertise into a professional setting.
+        
       </p>
     </section>
   );
